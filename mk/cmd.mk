e9654bc24c1ee21f9406d8aa1da28d11515a880a
Thomas Fossati <thomas.fossati@arm.com>
2021-11-03 18:40:33 +0000

    add cmd build template

---
+++ mk/cmd.mk
@@ -0,0 +1,24 @@
+# Copyright 2021 Contributors to the Veraison project.
+# SPDX-License-Identifier: Apache-2.0
+#
+# variables:
+# * CMD      - the name of the binary that is built
+# * SRCS     - the source files that CMD is build from
+# * CMD_DEPS - any extra dependency
+#
+# targets:
+# * all      - build the binary and save it to $(CMD)
+# * clean    - remove the generated binary
+
+.DEFAULT_GOAL := all
+
+ifndef CMD
+  $(error CMD must be set when including cmd.mk)
+endif
+
+$(CMD): $(SRCS) $(CMD_DEPS) ; go build -o $(CMD)
+
+CLEANFILES += $(CMD)
+
+.PHONY: all
+all: $(CMD)
