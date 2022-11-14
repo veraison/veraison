ae96554a1fd317c412a6a759a4294a41476793fd
Sergei Trofimov <sergei.trofimov@arm.com>
2022-11-14 10:46:00 +0000

    EOL commit.
    To make it clear that this is not the righ repository for Veraison code,
    delete everything and create a pointer to the correct locations.
    
    skip-checks:true
    Signed-off-by: Sergei Trofimov <sergei.trofimov@arm.com>

--- cmd/vts/README.md
+++
@@ -1,4 +0,0 @@
-# Veraison Trusted Services
-
-This is an implementation of the Veraison Trusted Services node taht provides
-the endorsements store and attestation scheme management.
--- frontend/test/send-token.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright 2022 Contributors to the Veraison project.
-# SPDX-License-Identifier: Apache-2.0
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-sessionPath=$(curl -s -i -X POST http://localhost:8080/challenge-response/v1/newSession?nonceSize=32 | grep Location | cut -f2 -d: | tr -d ' \r')
-
-curl -i -H "Content-Type: application/psa-attestation-token" -X POST http://localhost:8080$sessionPath -d "@$SCRIPT_DIR/psa-token.cbor"
